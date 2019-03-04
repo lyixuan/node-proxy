867f7bbce45da33f17c06bc5ce71d5199b1f5bdd
mzh <manzhenhua@sunlands.com>
2019-03-04 16:52:35 +0800

    修改port

--- src/proxy/index.js
+++ src/proxy/index.js
@@ -2,12 +2,21 @@ const proxyHostObj = {
     production: 'http://bd.ministudy.com/apis',
     development: 'http://172.16.117.65:8090',
 }
+var restream = function (proxyReq, req, res, options) {  // 在代理中将body中的参数序列化一下,否则将发送解析后的参数
+    if (req.body) {
+        let bodyData = JSON.stringify(req.body);
+        //     // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+        // proxyReq.setHeader('Content-Type', 'application/json');  // if you need
+        proxyReq.write(bodyData);
+    }
+}
 const proxyConfig = {
-    target: proxyHostObj['production'],  // target host
+    target: proxyHostObj[process.env.NODE_ENV],  // target host
     changeOrigin: true,               // needed for virtual hosted sites
     pathRewrite: {                     // 处理特殊需求使用,如果有特殊需求改动,否则按后端配置api路径使用
         '/proxy': '/',
     },
+    onProxyReq: restream,
 }
 
 
