9ff30e259fe774f54f1a0d379bee67225b05139c
mzh <manzhenhua@sunlands.com>
2019-03-04 10:00:42 +0800

    修改代理

--- app.js
+++ app.js
@@ -7,46 +7,62 @@ var proxy = require('http-proxy-middleware');
 var history = require('connect-history-api-fallback');
 // 用于格式化输出日志
 var logger = require('morgan');
-var proxyConfig=require('./src/proxy/index');
+var proxyConfig = require('./src/proxy/index');
 
 
 
 var app = express();
+
+//设置跨域请求头,
+// app.all('*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   if (req.method == 'OPTIONS') {
+//     //让options请求快速返回
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+//   next();
+// });
+
+
 app.use(logger('dev'));
 app.use(express.json());
 // 它基于body-parser解析传入的请求为urlencoded格式
 app.use(express.urlencoded({ extended: false }));
 // 处理传递cookie的解析问题
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 // 处理在body传递数据中间件
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // 测试接口
-app.get('/api',(req,res)=>{
-    res.cookie('testName', 'testValue');
-    return res.json({code:11122});
+app.get('api/test', (req, res) => {
+  res.cookie('testName', 'testValue');
+  return res.json({ text: '1112ss2' });
 })
 
 // 设置代理接口
-app.use('/proxy',proxy(proxyConfig));
-// 设置静态页面的history模式
+app.use('/proxy', proxy(proxyConfig));
+
+// 设置静态页面的history模式,根目录为/inspector,
 app.use(history());
-app.get("/inspector", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/inspector/index.html"))
-})
+app.use('/inspector', express.static(path.join(__dirname, 'inspector')))
+app.use(express.static(path.join(__dirname, 'inspector')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 // error handler
-app.use(function(err, req, res) {
+app.use(function (err, req, res) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.json({code:err.status,msg:"服务器异常"});
+  res.json({ code: err.status, msg: "服务器异常" });
 });
 
 module.exports = app;
--- src/proxy/index.js
+++ src/proxy/index.js
@@ -1,15 +1,15 @@
-const proxyHostObj={
+const proxyHostObj = {
     production: 'http://bd.ministudy.com/apis',
     development: 'http://172.16.117.65:8090',
 }
-const proxyConfig={
-        target: proxyHostObj[process.env.NODE_ENV],  // target host
-        changeOrigin: true,               // needed for virtual hosted sites
-        pathRewrite:{                     // 处理特殊需求使用,如果有特殊需求改动,否则按后端配置api路径使用
-                '/proxy': '/',
-},       
-    }
+const proxyConfig = {
+    target: proxyHostObj['production'],  // target host
+    changeOrigin: true,               // needed for virtual hosted sites
+    pathRewrite: {                     // 处理特殊需求使用,如果有特殊需求改动,否则按后端配置api路径使用
+        '/proxy': '/',
+    },
+}
 
 
 
-module.exports=proxyConfig;
+module.exports = proxyConfig;
